6f259116d1f660dab796a129cda603d397fdc829
Alex <alexrobertomata@hotmail.com>
2022-10-19 22:21:23 -0600

    Muestra usuarios en la vista

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -1,7 +1,41 @@
 
 $(document).ready(function() {
     //Al iniciar la pagina llama al metodo
-   //cargarUsuarios()
+    getUsers()
     alert("Alerta")
   //actualizarEmailUsuario()
 });
+
+  //Para devolver los Header
+    function getHeaders(){
+        return {
+                     'Accept': 'application/json',
+                     'Content-Type': 'application/json',
+                     'Authorization':localStorage.token
+                   };
+    }
+
+async function getUsers(){
+ const request = await fetch('api/usuarios', {
+    method: 'GET',
+    headers: getHeaders()
+  });
+  const usuarios = await request.json();
+
+    let listHtml='';
+    for (let user of usuarios){
+        let deleteButton =  '<button type="button" class="btn btn-icon btn-outline-danger">'
+                         +  '   <span class="tf-icons bx bx-trash-alt"></span>'
+                         +  '</button>';
+        let userHtml = '   <tr> '
+                     +  '  <td>'+user.id+'</td>'
+                     +  '  <td><strong>'+user.name+ ' ' +user.lastname+ '</strong></td>'
+                     +  '  <td>'+user.email+'</td> '
+                     +  '  <td>'+user.type+'</td>'
+                     +  '  <td>'+deleteButton+'</td>'
+                     +  '</tr>';
+        listHtml += userHtml;
+    }
+
+    document.querySelector('#table_user tbody').outerHTML = listadoHtml;
+}
