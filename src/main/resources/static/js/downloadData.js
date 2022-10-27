6e25760ce3e13a09476519f35abfb71ca58d4010
Manuel Alejandro Hurtado <manuhurtado_24@hotmail.com>
2022-10-26 22:12:58 -0600

    Generación de Reportes Funcionando

--- src/main/resources/static/js/downloadData.js
+++ src/main/resources/static/js/downloadData.js
@@ -14,15 +14,42 @@ async function clientData(){
 
       const clients = await request.json();
 
-      console.log(clients);
+      //Se convierte el json obtenido a un archivo CSV
+      const fileCsv = jsonToCsv(clients);
 
-      jsonToCsv(clients);
+      //Se manda a descarga el archivo con la información de los CLIENTES
+      download(fileCsv);
+}
 
-       console.log(jsonToCsv);
+async function docData(){
+      const request = await fetch('api/docs/clients', {
+        method: 'GET',
+        headers: getHeaders()
+      });
 
-      downloadInfo(jsonToCsv);
+      const docs = await request.json();
 
-      console.log(downloadInfo);
+      //Se convierte el json obtenido a un archivo CSV
+      const fileCsvDocs = jsonToCsv(docs);
+
+      //Se manda a descarga el archivo con la información de los CLIENTES
+      downloadDocs(fileCsvDocs);
+}
+
+
+async function addressData(){
+      const request = await fetch('api/address/clients', {
+        method: 'GET',
+        headers: getHeaders()
+      });
+
+      const addressC = await request.json();
+
+      //Se convierte el json obtenido a un archivo CSV
+      const fileCsvAddress = jsonToCsv(addressC);
+
+      //Se manda a descarga el archivo con la información de los CLIENTES
+      downloadDir(fileCsvAddress);
 }
 
 function jsonToCsv(items) {
@@ -45,7 +72,17 @@ function jsonToCsv(items) {
     return csv;
   }
 
-  function downloadInfo(data) {
+  function download(data) {
       var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
-  saveAs(blob, "clientes.csv");
+      saveAs(blob, "clientes.csv");
+  }
+
+  function downloadDocs(data) {
+      var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
+      saveAs(blob, "documentos.csv");
+  }
+
+    function downloadDir(data) {
+      var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
+      saveAs(blob, "direcciones.csv");
   }
