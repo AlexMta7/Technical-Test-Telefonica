853518e6da5e00f5054d90e4b19bc0e027a5e6aa
Alex <alexrobertomata@hotmail.com>
2022-10-27 13:58:32 -0600

    Algunos lineas de codigo que no se usaban fueron eliminadas

--- src/main/resources/static/js/auth-register-users.js
+++ src/main/resources/static/js/auth-register-users.js
@@ -5,16 +5,15 @@ $(document).ready(function() {
 });
 
 //Para devolver los Header
-    function getHeaders(){
-        return {
-                     'Accept': 'application/json',
-                     'Content-Type': 'application/json',
-                     //'Authorization':localStorage.token
-                   };
-    }
+function getHeaders() {
+    return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        //'Authorization':localStorage.token
+    };
+}
 
-async function addUser(){
-    event.preventDefault();
+async function addUser() {
     let user = {};
     user.name = document.getElementById("txtName").value;
     user.lastname = document.getElementById("txtLastname").value;
@@ -23,35 +22,34 @@ async function addUser(){
 
     let repeatPassword = document.getElementById("txtRepeatPassword").value;
 
-    if(user.name == "" && user.lastname == "" && user.email == "" && user.password == "" && repeatPassword == ""){
-        //alert("No puede ingresar un dato vació");
-        //location.reload();
-        return;
-    }
-    else{
-        user.type = "NO_ADMIN";
-    }
+    // if (user.name == "" && user.lastname == "" && user.email == "" && user.password == "" && repeatPassword == "") {
+    //     //alert("No puede ingresar un dato vació");
+    //     //location.reload();
+    //     return;
+    // }
+    // else {
+    //     user.type = "NO_ADMIN";
+    // }
 
-
-    if(repeatPassword != user.password){
+    if (repeatPassword != user.password) {
         alert("Las contraseñas no coinciden.")
         return;
     }
 
- const request = await fetch('api/users', {
-    method: 'POST',
-    headers: getHeaders(),
-    body: JSON.stringify(user)
-  });
-  const response = await request.text();
-  console.log(response);
-  if (response == 'OK'){
-            alert("Usuario ingresado correctamente");
-            location.reload();
-  }
-  else{
-       alert("Usuario ya existe");
-  }
+    const request = await fetch('api/users', {
+        method: 'POST',
+        headers: getHeaders(),
+        body: JSON.stringify(user)
+    });
+    const response = await request.text();
+    console.log(response);
+    if (response == 'OK') {
+        alert("User added successfully.");
+        location.reload();
+    }
+    else {
+        alert("User already exist.");
+    }
 }
 
 
