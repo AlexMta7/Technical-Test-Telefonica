20f58d380b68ca2fc9e16ccbc0c9c2f446c61844
Alex <alexrobertomata@hotmail.com>
2022-11-10 09:36:25 -0600

    Refactoreo de codigo

--- src/main/java/com/project/user/controllers/rest/UserController.java
+++ src/main/java/com/project/user/controllers/rest/UserController.java
@@ -19,26 +19,15 @@ public class UserController {
 
     @Autowired
     private JWTUtil jwtUtil;
+    /*
+     * Create
+     * Read
+     * Update
+     * Delete
+     */
 
-    @RequestMapping(value = "api/users/{id}", method = RequestMethod.GET)
-    public List<userModel> getUser(@PathVariable Long id) {
-        return userDao.getUser(id);
-    }
-
-    @RequestMapping(value = "api/users", method = RequestMethod.GET)
-    public List<userModel> getUsers(@RequestHeader(value = "Authorization") String token) {
-        
-        if(!validateToken(token)){
-            return null;
-        }
-        return userDao.getUsers();
-    }
-
-    @RequestMapping(value = "api/users/{id}", method = RequestMethod.DELETE)
-    public void deleteUsers(@PathVariable Long id){
-        userDao.deleteUsers(id);
-    }
-
+    // * Create
+    // Agrega un nuevo usuario con la contraseña encriptada
     @RequestMapping(value = "api/users", method = RequestMethod.POST)
     public String addUser(@RequestBody userModel user) {
         if (!userDao.verifyUser(user)) {
@@ -47,10 +36,30 @@ public class UserController {
             user.setPassword(hash);
             userDao.addUser(user);
             return "OK";
-        } 
-        else {
+        } else {
             return "FAIL";
         }
+    }
+
+    // * Read
+    // Obtiene usuarios si existe un token
+    @RequestMapping(value = "api/users", method = RequestMethod.GET)
+    public List<userModel> getUsers(@RequestHeader(value = "Authorization") String token) {
+
+        if (!validateToken(token)) {
+            return null;
+        }
+        return userDao.getUsers();
+    }
+
+    @RequestMapping(value = "api/users/{id}", method = RequestMethod.GET)
+    public List<userModel> getUser(@PathVariable Long id) {
+        return userDao.getUser(id);
+    }
+
+    @RequestMapping(value = "api/users/{id}", method = RequestMethod.DELETE)
+    public void deleteUsers(@PathVariable Long id) {
+        userDao.deleteUsers(id);
     }
 
     @RequestMapping(value = "api/users", method = RequestMethod.PUT)
@@ -58,10 +67,10 @@ public class UserController {
         userDao.updateUser(user);
         return "OK";
         // if (userDao.verifyUser(user)) {
-        //     userDao.updateUser(user);
-        //     return "OK";
+        // userDao.updateUser(user);
+        // return "OK";
         // } else {
-        //     return "FAIL";
+        // return "FAIL";
         // }
     }
 
@@ -70,39 +79,40 @@ public class UserController {
         return userID != null;
     }
 
-
     /*
-    @RequestMapping(value = "api/users", method = RequestMethod.GET)
-    public List<userModel> getUsers(){
-        return UserDao.getUsers();
-    }
-
+     * @RequestMapping(value = "api/users", method = RequestMethod.GET)
+     * public List<userModel> getUsers(){
+     * return UserDao.getUsers();
+     * }
+     * 
      */
 
-    /* Funciona, devuelve la lista
-    @RequestMapping(value = "api/usuario123", method = RequestMethod.GET)
-    public List<userModel> getUsuario(){
-        List<userModel> users = new ArrayList<>();
-        userModel usuario = new userModel();
-        usuario.setId(123L);
-        usuario.setName("Alex");
-        usuario.setLastname("Mata");
-        usuario.setEmail("alexmata@hotmail.com");
-        usuario.setPassword("1234567");
-        usuario.setType("admin");
-
-        userModel usuario2 = new userModel();
-        usuario2.setId(123L);
-        usuario2.setName("Alex2");
-        usuario2.setLastname("Mata2");
-        usuario2.setEmail("alexmata2@hotmail.com");
-        usuario2.setPassword("12345678");
-        usuario2.setType("noAdmin");
-
-        users.add(usuario);
-        users.add(usuario2);
-        return users;
-    }
+    /*
+     * Funciona, devuelve la lista
+     * 
+     * @RequestMapping(value = "api/usuario123", method = RequestMethod.GET)
+     * public List<userModel> getUsuario(){
+     * List<userModel> users = new ArrayList<>();
+     * userModel usuario = new userModel();
+     * usuario.setId(123L);
+     * usuario.setName("Alex");
+     * usuario.setLastname("Mata");
+     * usuario.setEmail("alexmata@hotmail.com");
+     * usuario.setPassword("1234567");
+     * usuario.setType("admin");
+     * 
+     * userModel usuario2 = new userModel();
+     * usuario2.setId(123L);
+     * usuario2.setName("Alex2");
+     * usuario2.setLastname("Mata2");
+     * usuario2.setEmail("alexmata2@hotmail.com");
+     * usuario2.setPassword("12345678");
+     * usuario2.setType("noAdmin");
+     * 
+     * users.add(usuario);
+     * users.add(usuario2);
+     * return users;
+     * }
      */
 
 }
