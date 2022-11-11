7b7869d608c52c8256fb89d64d85fe9e46d032ab
Alex <alexrobertomata@hotmail.com>
2022-11-11 10:28:39 -0600

    Model para logs

---
+++ src/main/java/com/project/user/models/logModel.java
@@ -0,0 +1,27 @@
+package com.project.user.models;
+
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "logs")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class logModel {
+
+    @Id
+    @Column(name = "log_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "log_client_id")
+    private Long client_id;
+
+    @Column(name = "log_action")
+    private String action;
+
+    @Column(name = "log_date")
+    private String date;
+}
